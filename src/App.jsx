d8b3f274dc598e3d7030b112cac5f235f2ff5d12
jakareya76 <jakareya1306@gmail.com>
2024-03-08 16:35:34 +0600

    learn jsx and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  const world = "World";
   return (
     <div className="container mx-auto">
       <Navbar />
+
+      <div className="font-mono text-xl font-semibold">
+        <h2>Hello {world}</h2>
+      </div>
     </div>
   );
 };
