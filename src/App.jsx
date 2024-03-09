bd4136cfad9b9d6f2c7f2925804cbf6f59ab3bba
jakareya76 <jakareya1306@gmail.com>
2024-03-09 13:10:40 +0600

    learn event handler in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,39 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   const world = "World";
+
+  const navList = ["Home", "About", "Blog", "Contact"];
+
+  const handleClick = (e) => {
+    alert("button clicked");
+    console.log(e.target);
+  };
+
+  const handleSum = (num) => {
+    alert(num + 5);
+  };
+
   return (
     <div className="container mx-auto">
-      <Navbar />
+      <Navbar navList={navList} />
 
       <div className="font-mono text-xl font-semibold">
         <h2>Hello {world}</h2>
       </div>
+
+      <button
+        onClick={handleClick}
+        className="px-5 py-3 font-semibold bg-gray-200 rounded-xl text-zinc-800"
+      >
+        Click Me
+      </button>
+
+      <button
+        onClick={() => handleSum(5)}
+        className="px-5 py-3 mx-5 my-5 font-semibold bg-gray-200 rounded-xl text-zinc-800"
+      >
+        Add Sum
+      </button>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-const Navbar = () => {
+const Navbar = ({ navList }) => {
   return (
     <nav className="flex items-center justify-between p-8">
       <div>
@@ -6,9 +6,9 @@ const Navbar = () => {
       </div>
       <div>
         <ul className="flex items-center justify-center space-x-4">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          {navList.map((list) => {
+            return <li key={list}>{list}</li>;
+          })}
         </ul>
       </div>
     </nav>
